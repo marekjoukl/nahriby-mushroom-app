a8f162b5b60fd9b732eab1eb4c4ba9852752066a
Marek Joukl <jouklmarek@seznam.cz>
2024-11-09 12:33:50 +0100

    fix: map marker

--- src/features/map/CreateLocation.jsx
+++ src/features/map/CreateLocation.jsx
@@ -3,6 +3,7 @@ import { Form, redirect, useSearchParams } from "react-router-dom";
 import { createLocation } from "../../api/apiMap";
 import BackButton from "../../ui/BackButton";
 import { getMushrooms } from "../../api/apiMushrooms";
+import Button from "../../ui/Button";
 
 function CreateLocation() {
   const [mushrooms, setMushrooms] = useState([]);
@@ -41,6 +42,7 @@ function CreateLocation() {
         style={{
           color: i < locationData.stars ? "yellow" : "gray",
           fontSize: "2.5rem",
+          padding: "0 0.35rem",
         }}
       >
         ★
@@ -60,99 +62,84 @@ function CreateLocation() {
   return (
     <Form
       method="POST"
-      className="mx-auto max-w-md space-y-4 rounded-lg bg-white p-6 pb-16 shadow-lg"
+      className="mx-auto max-w-md space-y-4 bg-bg-primary p-6 pb-16 pt-12 text-white shadow-lg"
     >
-      <BackButton iconType="x" />
-      <h2 className="text-center text-xl font-semibold">
+      <BackButton iconType="x" navigateTo="/map" />
+      <h2 className="text-center text-xl font-semibold text-white">
         Create a New Location
       </h2>
 
       <div>
-        <label className="mb-2 block text-sm font-medium text-gray-700">
-          Name
-        </label>
-        <input
-          type="text"
-          name="name"
-          className="w-full rounded border border-gray-300 p-2 focus:ring focus:ring-green-200"
-          required
-        />
-      </div>
-
-      <div>
         <div className="flex justify-center">{renderStars()}</div>
-        {/* Hidden field to store star rating */}
         <input type="hidden" name="stars" value={locationData.stars} />
       </div>
 
       <div>
-        <label className="mb-2 block text-sm font-medium text-gray-700">
-          Latitude
-        </label>
         <input
           type="text"
-          name="lat"
-          defaultValue={locationData.lat}
-          className="w-full rounded border border-gray-300 p-2 focus:ring focus:ring-green-200"
+          name="name"
+          placeholder="Name"
+          className="w-full rounded-xl border border-gray-300 p-3 text-black focus:ring focus:ring-green-200"
           required
         />
       </div>
 
-      <div>
-        <label className="mb-2 block text-sm font-medium text-gray-700">
-          Longitude
-        </label>
-        <input
-          defaultValue={locationData.lng}
-          type="text"
-          name="lng"
-          className="w-full rounded border border-gray-300 p-2 focus:ring focus:ring-green-200"
-          required
-        />
+      <div className="flex items-center justify-center space-x-2 text-gray-300">
+        <label className="text-xs font-bold tracking-widest">LAT:</label>
+        <p className="text-xs font-light tracking-widest">
+          {Number(locationData.lat)?.toFixed(7)}
+        </p>
+        <span className="mx-1">,</span>{" "}
+        <label className="text-xs font-bold tracking-widest">LNG:</label>
+        <p className="text-xs font-light tracking-widest">
+          {Number(locationData.lng)?.toFixed(7)}
+        </p>
       </div>
+      <input type="hidden" name="lat" value={locationData.lat} />
+      <input type="hidden" name="lng" value={locationData.lng} />
 
       <div>
-        <label className="mb-2 block text-sm font-medium text-gray-700">
-          Image
-        </label>
-        <input
-          type="text"
-          name="image_url"
-          className="w-full rounded border border-gray-300 p-2 focus:ring focus:ring-green-200"
-        />
-      </div>
-
-      <div>
-        <label className="mb-2 block text-sm font-medium text-gray-700">
-          Description
-        </label>
         <textarea
           name="description"
-          className="w-full rounded border border-gray-300 p-2 focus:ring focus:ring-green-200"
+          className="w-full rounded-xl border border-gray-300 p-4 text-black focus:ring focus:ring-green-200"
           rows="3"
-          placeholder="Enter a short description of the location"
+          placeholder="Write something..."
           required
         />
       </div>
+
       <div>
-        <label className="mb-2 block text-sm font-medium text-gray-700">
-          Select Mushrooms
+        <input
+          placeholder="Image URL"
+          type="text"
+          name="image_url"
+          className="w-full rounded-xl border border-gray-300 p-3 text-black focus:ring focus:ring-green-200"
+        />
+      </div>
+
+      <div>
+        <label className="mb-2 block text-sm font-medium">
+          Mushrooms in this location:
         </label>
         <div className="flex flex-wrap gap-2">
-          {mushrooms.map((mushroom) => (
-            <button
-              key={mushroom.id}
-              type="button"
-              onClick={() => toggleMushroomSelection(mushroom.id)}
-              className={`rounded-full border px-4 py-2 ${
-                locationData.mushrooms.includes(mushroom.id)
-                  ? "bg-green-500 text-white"
-                  : "bg-gray-200 text-gray-700"
-              }`}
-            >
-              {mushroom.name}
-            </button>
-          ))}
+          {mushrooms.length > 0 ? (
+            mushrooms.map((mushroom) => (
+              <button
+                key={mushroom.id}
+                type="button"
+                onClick={() => toggleMushroomSelection(mushroom.id)}
+                className={`rounded-full border px-4 py-2 ${
+                  locationData.mushrooms.includes(mushroom.id)
+                    ? "bg-green-500 text-white"
+                    : "bg-gray-200 text-gray-700"
+                }`}
+              >
+                {mushroom.name}
+              </button>
+            ))
+          ) : (
+            <p>Loading mushrooms...</p>
+          )}
         </div>
       </div>
 
@@ -163,12 +150,14 @@ function CreateLocation() {
       />
       <input type="hidden" name="author" value={locationData.author} />
 
-      <button
-        type="submit"
-        className="w-full rounded bg-green-500 px-4 py-2 font-semibold text-white hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
-      >
-        Submit
-      </button>
+      <div className="flex justify-center">
+        <button
+          type="submit"
+          className="rounded-full bg-navbar-active px-4 py-2 text-white hover:bg-[#0cd784] focus:outline-none focus:ring-2 focus:ring-[#0FE596] focus:ring-opacity-50"
+        >
+          Publish
+        </button>
+      </div>
     </Form>
   );
 }
@@ -176,7 +165,6 @@ function CreateLocation() {
 export async function action({ request }) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-  console.log(data);
   try {
     const newLocation = await createLocation(data);
 
--- src/features/map/LocationMarker.jsx
+++ src/features/map/LocationMarker.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Marker, Popup } from "react-leaflet";
-import { getUser } from "../../api/apiUsers"; // Import getUser function
+import { getUser } from "../../api/apiUsers";
 import { useNavigate } from "react-router-dom";
 
 function LocationMarker({ location }) {
@@ -21,44 +21,48 @@ function LocationMarker({ location }) {
   }, [location.author]);
 
   return (
-    <Marker key={location.id} position={[location.lat, location.lng]}>
-      <Popup maxWidth={250} offset={[0, -10]} className="custom-popup">
-        <div className="p-2">
-          {author && (
-            <div className="mb-2 flex items-center">
-              <img
-                src={author.image_url}
-                alt={`${author.name}'s profile`}
-                className="mr-2 h-8 w-8 rounded-full"
-              />
-              <span className="font-semibold text-gray-800">{author.name}</span>
+    (location.lat, location.lng) && (
+      <Marker key={location.id} position={[location.lat, location.lng]}>
+        <Popup maxWidth={250} offset={[0, -10]} className="custom-popup">
+          <div className="p-2">
+            {author && (
+              <div className="mb-2 flex items-center">
+                <img
+                  src={author.image_url}
+                  alt={`${author.name}'s profile`}
+                  className="mr-2 h-8 w-8 rounded-full"
+                />
+                <span className="font-semibold text-gray-800">
+                  {author.name}
+                </span>
+              </div>
+            )}
+
+            <h2 className="text-lg font-bold text-gray-800">{location.name}</h2>
+
+            <div className="my-1 flex items-center">
+              {Array.from({ length: 5 }).map((_, index) => (
+                <span
+                  key={index}
+                  className={`text-lg ${
+                    index < location.stars ? "text-yellow-500" : "text-gray-300"
+                  }`}
+                >
+                  ★
+                </span>
+              ))}
             </div>
-          )}
 
-          <h2 className="text-lg font-bold text-gray-800">{location.name}</h2>
-
-          <div className="my-1 flex items-center">
-            {Array.from({ length: 5 }).map((_, index) => (
-              <span
-                key={index}
-                className={`text-lg ${
-                  index < location.stars ? "text-yellow-500" : "text-gray-300"
-                }`}
-              >
-                ★
-              </span>
-            ))}
+            <p
+              onClick={handleClick}
+              className="cursor-pointer text-right text-blue-500 hover:text-blue-600"
+            >
+              View Details
+            </p>
           </div>
-
-          <p
-            onClick={handleClick}
-            className="cursor-pointer text-right text-blue-500 hover:text-blue-600"
-          >
-            View Details
-          </p>
-        </div>
-      </Popup>
-    </Marker>
+        </Popup>
+      </Marker>
+    )
   );
 }
 
