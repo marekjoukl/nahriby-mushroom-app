a6b85541f59a1a3f4fc470caf15c0f2e9c5423b4
astrigac <aurel.strigac@proton.me>
2024-12-12 00:06:05 +0100

    added header

--- src/features/user/User.jsx
+++ src/features/user/User.jsx
@@ -1,5 +1,3 @@
-// src/components/User.jsx
-
 import { useLoaderData, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import {
@@ -9,6 +7,7 @@ import {
   getUserRecipes,
 } from "../../api/apiUsers";
 import { FiSettings, FiMapPin, FiBookmark, FiPlus } from "react-icons/fi";
+import Header from "../../ui/Header";
 
 function User() {
   const { user, locations, mushrooms, recipes } = useLoaderData();
@@ -88,16 +87,19 @@ function User() {
   };
 
   return (
-    <div className="min-h-screen w-full bg-bg-primary p-5 pb-16 text-center font-sans text-white">
-      <div className="flex items-center justify-between">
-        <h1 className="m-0 font-serif text-2xl">Your profile</h1>
-        <FiSettings
-          className="cursor-pointer text-2xl"
-          onClick={handleSettingsClick}
-        />
-      </div>
+    <div className="min-h-screen w-full bg-bg-primary p-5 pb-20 text-center font-sans text-white">
+      {/* Header Section with Title and Icons */}
+      <Header 
+        title="Your Profile"
+        backButtonFlag={false}
+        RightIcon1={FiBookmark}
+        onRightIcon1Click={handleBookmarkClick}
+        RightIcon2={FiSettings}
+        onRightIcon2Click={handleSettingsClick}>
+      </Header>
 
-      <div className="mt-5 flex flex-col items-center">
+      {/* Profile Section */}
+      <div className="mt-20 flex flex-col items-center">
         <div className="h-32 w-32 overflow-hidden rounded-full border-4 border-white">
           <img
             src={user.image_url}
@@ -113,17 +115,14 @@ function User() {
             <span>{user.country}</span>
           </div>
         </div>
-
-        <FiBookmark
-          className="mt-4 cursor-pointer text-2xl text-white"
-          onClick={handleBookmarkClick}
-        />
       </div>
 
+      {/* Posts Count */}
       <div className="mt-4 text-lg font-bold text-green-500">
         {locations.length + mushrooms.length + recipes.length} Posts
       </div>
 
+      {/* Tabs Section */}
       <div className="mt-5 flex justify-around border-b border-green-500 pb-2">
         <button
           className={`p-2 text-lg ${
@@ -157,7 +156,8 @@ function User() {
         </button>
       </div>
 
-      <div className="mt-5 flex justify-center gap-3 pb-6">
+      {/* Content Section */}
+      <div className="mt-5 grid grid-cols-3 gap-2 justify-items-center">
         <div
           className="flex h-32 w-24 cursor-pointer items-center justify-center rounded-md border border-white hover:bg-bg-secondary"
           onClick={handleAddClick}
