b516153637b26a6772cb1aa8315f47672cc052b6
Marek Joukl <jouklmarek@seznam.cz>
2024-12-15 22:43:55 +0100

    chore: add project header comments to AppLayout, Error, and PlusButton components

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,16 @@
+/**
+ * Project: ITU - Mushroom app
+ * Author: Marek Joukl (xjoukl00)
+ * Date: 15.12. 2024
+ */
+
 import { Outlet } from "react-router-dom";
 import PageNav from "./PageNav";
 
 function AppLayout() {
   return (
     <>
-      <div className="bg-green-950 min-h-screen text-white">
+      <div className="min-h-screen bg-green-950 text-white">
         <Outlet />
         <PageNav />
       </div>
